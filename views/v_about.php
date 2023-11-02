60bbf5ee6ee61a95c2fccf89a61669ace10ded99
dina0209 <145654276+dina0209@users.noreply.github.com>
2023-11-02 16:23:13 +0700

    Add files via upload

---
+++ views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?><h/1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dina</li>
+        <li>Nama Belakang : Syahira Khansa</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Perumahan Mutiara Bekasi Jaya Blok G6/7 Kec. Cibarusah Kab. Bekasi</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Voli</li>
+        <li>Badminton</li>
+    </ul>
+</section>
